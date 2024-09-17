51db713915bf2a26dac4bdc3e7199ceb8871302d
Dan Welling <dwelling@umich.edu>
2024-09-17 10:36:09 -0400

    First example of numerical differencing.

---
+++ diffs.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+'''
+Let's explore numerical differencing! Yay! Everyone say yay right now.
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.style.use('fivethirtyeight')
+
+
+def fwd_diff(y, dx):
+    '''Return forward diff approx of 1st derivative'''
+
+    dydx = np.zeros(y.size)
+
+    # Forward diff:
+    dydx[:-1] = (y[1:] - y[:-1]) / dx
+
+    # last point:
+    dydx[-1] = (y[-1] - y[-2]) / dx
+
+    return dydx
+
+deltax = 0.5
+x = np.arange(0, 4*np.pi, deltax)
+
+fx = np.sin(x)
+fxd1 = np.cos(x)
+
+fig, ax = plt.subplots(1, 1)
+ax.plot(x, fx, '.', alpha=.6, label=r'$f(x) = \sin(x)$')
+ax.plot(x, fxd1, label=r'$f(x) = \frac{d\sin(x)}{dx}$')
+
+ax.plot(x, fwd_diff(fx, deltax), label='Fwd Diff')
+
+ax.legend(loc='upper right')
