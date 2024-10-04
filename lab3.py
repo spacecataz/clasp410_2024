a2a6c1af76147c5f4fbc04ef3a0b4853e4338b21
Dan Welling <dwelling@umich.edu>
2024-10-03 21:16:19 -0400

    Completed and verified our n_layer_atmos function.

--- lab3.py
+++ lab3.py
@@ -3,20 +3,15 @@
 A set of tools and routines for solving the N-layer atmosphere energy
 balance problem and perform some useful analysis.
 
-TO REPRODUCE FIGURES IN LAB WRITE-UP: DO THE FOLLOWING
-blah
-blah
-blah
 '''
 
 import numpy as np
-import matplotlib.pyplot as plt
 
- # Define some useful constants here.
+#  Define some useful constants here.
 sigma = 5.67E-8  # Steffan-Boltzman constant.
 
 
-def n_layer_atmos(N, epsilon, S0=1350, albedo=0.33):
+def n_layer_atmos(N, epsilon, S0=1350, albedo=0.33, debug=False):
     '''
     Solve the n-layer atmosphere problem and return temperature at each layer.
 
@@ -32,6 +27,12 @@ def n_layer_atmos(N, epsilon, S0=1350, albedo=0.33):
         Set the incoming solar shortwave flux in Watts/m^2.
     debug : boolean, default=False
         Turn on debug output.
+
+    Returns
+    -------
+    temps : Numpy array of size N+1
+        Array of temperatures from the Earth's surface (element 0) through
+        each of the N atmospheric layers, from lowest to highest.
     '''
 
     # Create matrices:
@@ -39,12 +40,39 @@ def n_layer_atmos(N, epsilon, S0=1350, albedo=0.33):
     b = np.zeros(N+1)
     b[0] = -S0/4 * (1-albedo)
 
-    # Populate our A matrix.
+    if debug:
+        print(f"Populating N+1 x N+1 matrix (N = {N})")
+
+    # Populate our A matrix piece-by-piece.
     for i in range(N+1):
         for j in range(N+1):
+            if debug:
+                print(f"Calculating point i={i}, j={j}")
+            # Diagonal elements are always -2 ('cept at Earth's surface.)
             if i == j:
                 A[i, j] = -1*(i > 0) - 1
+                # print(f"Result: A[i,j]={A[i,j]}")
             else:
+                # This is the pattern we solved for in class!
                 m = np.abs(j-i) - 1
                 A[i, j] = epsilon * (1-epsilon)**m
+    # At Earth's surface, epsilon =1, breaking our pattern.
+    # Divide by epsilon along surface to get correct results.
     A[0, 1:] /= epsilon
+
+    # Verify our A matrix.
+    if debug:
+        print(A)
+
+    # Get the inverse of our A matrix.
+    Ainv = np.linalg.inv(A)
+
+    # Multiply Ainv by b to get Fluxes.
+    fluxes = np.matmul(Ainv, b)
+
+    # Convert fluxes to temperatures.
+    # Fluxes for all atmospheric layers
+    temps = (fluxes/epsilon/sigma)**0.25
+    temps[0] = (fluxes[0]/sigma)**0.25  # Flux at ground: epsilon=1.
+
+    return temps
